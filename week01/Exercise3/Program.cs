7b878fec016da8f1af2ab220f656650d9c006b0f
asarepro <langfordasare@gmail.com>
2025-07-05 03:32:37 +0000

    finished

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Runtime.Versioning;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        // Random number generat
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 20);
+
+        int guess = -1;
+
+        // while loop
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
+    
+        
+        
     }
 }
